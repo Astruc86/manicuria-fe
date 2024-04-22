e8517e812b9772627bf82f9f34d2e80758816cf0
Astruc86 <aldisto.gale@gmail.com>
2024-04-22 20:42:26 -0300

    cambio de nombre de footer.css

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,8 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MyCarousel from '../components/carousel/Carousel'
+
 export const HomeScreen = () => {
   return (
     <>
       <h1>HomeScreen</h1>
       <hr />
+      <MyCarousel />
+      <div className="d-grid gap-2 col-2 mx-auto">
+        <Link to="/turno">
+          <button type="button" className="btn btn-outline-primary">
+            Reservar turno
+          </button>
+        </Link>
+      </div>
     </>
   );
 };
