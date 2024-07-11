028beb5cc15b021de2cd09f9e247b2b86dc5293f
Astruc86 <aldisto.gale@gmail.com>
2024-07-11 18:59:36 -0300

    logica para mostrar actualizada el resumen parcial dependiendo de la seleccion en el stepper

--- src/components/resumen/Resumen.jsx
+++ src/components/resumen/Resumen.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
+import dayjs from "dayjs";
 import './resumen.css';
+import {
+  useSeleccionServicio,
+  useProfesionalSeleccionado,
+  useSeleccionHorario,
+  useSeleccionDia,
+  useActiveStep,
+} from "../../context/StepperContext";
 
 const Resumen = () => {
+  const { activeStep } = useActiveStep();
+  const { seleccionServicio } = useSeleccionServicio();
+  const { profesionalSeleccionado } = useProfesionalSeleccionado();
+  const { seleccionHorario } = useSeleccionHorario();
+  const { seleccionDia } = useSeleccionDia();
+
+  const servicio = seleccionServicio?.nombre;
+  const precio =`$${seleccionServicio.precio}`;
+  const duracion = seleccionServicio?.duracion || 30;
+  const profesional = profesionalSeleccionado?.nombre;
+  const dia =  dayjs(seleccionDia).format('DD-MM-YYYY');
+
   return (
     <div className="resumen">
       <h2>Resumen</h2>
-      <p>Servicio: servicio seleccionado</p>
-      <p>Precio: $20000</p>
-      <p>Duración: 30 min</p>
-      <p>Profesional: profesional seleccionado</p>
-      <p>Día: DD/MM/AAAA</p>
+      {activeStep >= 0 && (
+        <p>Servicio: {servicio}</p>
+      )}
+      {activeStep >= 1 && (
+        <>
+          <p>Precio: {precio}</p>
+          <p>Duración: {duracion} min</p>
+        </>
+      )}
+      {activeStep >= 2 && (
+        <p>Profesional: {profesional}</p>
+      )}
+      {activeStep >= 3 && (
+        <>
+          <p>Día: {dia}</p>
+        </>
+      )}
     </div>
   );
 };
