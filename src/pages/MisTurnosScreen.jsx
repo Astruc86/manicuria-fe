b1aaf852517bf9e4357cafb1a1d65a2f0298e246
lopezsol <mariasollopez23@gmail.com>
2024-07-20 05:44:58 -0300

    se borra codigo en misturnosscreen

--- src/pages/MisTurnosScreen.jsx
+++ src/pages/MisTurnosScreen.jsx
@@ -1,21 +1,9 @@
 import React from "react";
-import { useTurno } from "../context/TurnosContext";
 
 export const MisTurnosScreen = () => {
-  console.log("MisTurnosScreen");
-  const { turnos, agregarTurno } = useTurno();
-  console.log("turno", turnos);
   return (
     <>
-      Mis Turnos screen
-      {turnos &&
-        turnos.map((turno) => (
-          <div key={turno.id}>
-            <p>{turno.nombreProfesional}</p>
-            <p>{turno.fechaCita}</p>
-            <p>{turno.horaCita}</p>
-          </div>
-        ))}
+      <h1>Mis turnos screen</h1>
     </>
   );
 };
