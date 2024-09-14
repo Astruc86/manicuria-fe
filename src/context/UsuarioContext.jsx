843564f1201d2293148a4f485f7b44b552c9d5a8
lopezsol <mariasollopez23@gmail.com>
2024-09-14 03:30:05 -0300

    se agrega usuario al session storage

--- src/context/UsuarioContext.jsx
+++ src/context/UsuarioContext.jsx
@@ -1,12 +1,20 @@
 import { createContext, useState, useContext } from "react";
+export const usuarioInitialState = JSON.parse(
+  window.sessionStorage.getItem("usuario")
+) || { dni: "", rol: 0 };
 
+export const actualizarSessionStorage = (usuario) => {
+  window.sessionStorage.setItem("usuario", JSON.stringify(usuario));
+};
 const UsuarioContext = createContext();
 
 export const UsuarioProvider = ({ children }) => {
   //0: cliente, 1: empresa, 2:clienteLogueado
-  const [usuario, setUsuario] = useState({ dni: "", rol: 0 });
+  const [usuario, setUsuario] = useState(usuarioInitialState);
+
   const modificarUsuario = (nuevoUsuario) => {
     if (!nuevoUsuario.dni || !nuevoUsuario.rol) return;
+    actualizarSessionStorage(nuevoUsuario)
     setUsuario(nuevoUsuario);
   };
 
